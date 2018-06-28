7242e1203ed83bfe079f4ad21d305d2a17225342
Eric Stokes <letaris@gmail.com>
2018-06-28 17:19:48 -0400

    preliminary test for store

--- src/resolver_store.rs
+++ src/resolver_store.rs
@@ -124,3 +124,50 @@ impl Store {
     self.0.iter()
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  static ADDRS: [(&str, &str)] = [
+    ("/app/test/app0/v0", "127.0.0.1:100"),
+    ("/app/test/app0/v1", "127.0.0.1:100"),
+    ("/app/test/app0/v0", "127.0.0.1:101"),
+    ("/app/test/app0/v1", "127.0.0.1:101"),
+    ("/app/test/app1/v2", "127.0.0.1:105"),
+    ("/app/test/app1/v3", "127.0.0.1:105"),
+    ("/app/test/app1/v4", "127.0.0.1:105")
+  ];
+
+  #[test]
+  fn test() {
+    let mut store = Store::new();
+    let mut n = 0;
+    for (path, addr) in ADDRS {
+      let addr = addr.parse::<SocketAddr>()?;
+      let path = Path::from(path);
+      store.publish(path.clone(), addr);
+      if n < 2 && store.resolve(&path).len() != 1 {
+        panic!("published path wrong {}", path)
+      } else if n < 4 && store.resolve(&path).len() != 2 {
+        panic!("published path wrong {}", path)
+      } else if n >= 4 && store.resolve(&path).len() != 1 {
+        panic!("published path wrong {}", path)
+      }
+      n += 1
+    }
+    let paths = store.list(&Path::from("/app/test"));
+    if paths.len() != 2 { panic!(); }
+    if path[0].as_ref() != "app0" { panic!("list00 {}", path[0]) }
+    if path[1].as_ref() != "app1" { panic!("list01 {}", path[1]) }
+    let paths = store.list(&Path::from("/app/test/app0"));
+    if paths.len() != 2 { panic!() }
+    if path[0].as_ref() != "v0" { panic!("list10, {}", path[0]) }
+    if path[1].as_ref() != "v1" { panic!("list11, {}", path[1]) }
+    let paths = store.list(&Path::from("/app/test/app1"));
+    if paths.len() != 3 { panic!() }
+    if path[0].as_ref() != "v2" { panic!("list20, {}", path[0]) }
+    if path[1].as_ref() != "v3" { panic!("list21, {}", path[1]) }
+    if path[2].as_ref() != "v4" { panic!("list22, {}", path[2]) }
+  }
+}
