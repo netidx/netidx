f127880ef86e1d94dccfb330a6515e86b59ed50b
Eric Stokes <letaris@gmail.com>
2020-02-16 19:01:25 -0500

    change subscriber to use durable subscriptions, and output and input json

--- json-pubsub-tools/src/subscriber.rs
+++ json-pubsub-tools/src/subscriber.rs
@@ -1,35 +1,34 @@
 use json_pubsub::{
     path::Path,
-    subscriber::Subscriber, 
-    utils::{Batched, BatchItem, BytesDeque, BytesWriter},
+    subscriber::{Subscriber, DUVal},
+    utils::{Batched, BatchItem, BytesWriter},
     config,
 };
 use futures::{
     prelude::*,
+    stream::{self, SelectAll, FusedStream},
     select,
 };
 use tokio::{
-    task, time, runtime::Runtime,
+    runtime::Runtime,
     io::{self, BufReader, AsyncWriteExt, AsyncBufReadExt},
-    sync::{oneshot, mpsc},
 };
 use std::{
     collections::{HashMap, HashSet},
     str::{FromStr, from_utf8},
     result::Result,
-    cell::RefCell,
-    time::Duration,
 };
 use bytes::{BytesMut, Bytes};
+use serde_json::Value;
 
-fn mpv_to_json(v: rmpv::Value) -> serde_json::Value {
+fn mpv_to_json(v: rmpv::Value) -> Value {
     use rmpv::{Value as Rv, Utf8String};
     use serde_json::{Value as Jv, Number, Map};
     use std::num::FpCategory;
     let cvt_str = |s: Utf8String| -> String {
         String::from_utf8_lossy(s.as_bytes()).into()
     };
-    let cvt_float = |f: f64| -> serde_json::Value {
+    let cvt_float = |f: f64| -> Value {
         match Number::from_f64(f) {
             Some(n) => Jv::Number(n),
             None => match f64::classify(f) {
@@ -78,127 +77,45 @@ fn mpv_to_json(v: rmpv::Value) -> serde_json::Value {
     }
 }
 
-thread_local! {
-    static BUF: RefCell<BytesMut> = RefCell::new(BytesMut::with_capacity(512));
-}
-
-fn json_encode(v: &serde_json::Value) -> Result<Bytes, serde_json::Error> {
-    BUF.with(|buf| {
-        let mut b = buf.borrow_mut();
-        serde_json::to_writer(&mut BytesWriter(&mut *b), &v)?;
-        Ok(b.split().freeze())
-    })
-}
-
-pub fn str_encode(t: &str) -> Bytes {
-    BUF.with(|buf| {
-        let mut b = buf.borrow_mut();
-        b.extend_from_slice(t.as_bytes());
-        b.split().freeze()
-    })
-}
-
-macro_rules! ret {
-    ($e:expr) => {
-        match $e {
-            Ok(r) => r,
-            Err(_) => return,
-        }
-    };
-}
-
-async fn run_subscription(
-    path: Path,
-    subscriber: Subscriber,
-    stop: oneshot::Receiver<()>,
-    mut out: mpsc::Sender<(Bytes, Bytes)>,
-) {
-    let path_b = str_encode(path.replace('|', r"\|").as_str());
-    let mut stop = stop.fuse();
-    loop {
-        let sub = {
-            let mut tries: u64 = 0;
-            loop {
-                select! {
-                    _ = stop => return,
-                    r = subscriber.subscribe_val_ut(path.clone()).fuse() => match r {
-                        Ok(sub) => break sub,
-                        Err(e) => {
-                            let m = str_encode(&format!("{}", e));
-                            ret!(out.send((path_b.clone(), m)).await);
-                            tries += 1;
-                            time::delay_for(Duration::from_secs(tries)).await;
-                        }
-                    }
-                }
-            }
-        };
-        let mut updates = sub.updates(true).fuse();
-        loop {
-            select! {
-                _ = stop => return,
-                m = updates.next() => match m {
-                    None => break,
-                    Some(m) => {
-                        let v = match rmpv::decode::value::read_value(&mut &*m) {
-                            Ok(v) => try_brk!("json", json_encode(&mpv_to_json(v))),
-                            Err(_) => match from_utf8(&*m) {
-                                Ok(_) => m,
-                                Err(_) => str_encode(format!("{:?}", m).as_str()),
-                            }
-                        };
-                        ret!(out.send((path_b.clone(), v)).await);
-                    }
-                }
-            }
-        }
-    }
-}
-
-async fn output_vals(msgs: mpsc::Receiver<(Bytes, Bytes)>) {
-    let fsep = str_encode("|");
-    let rsep = str_encode("\n");
-    let mut msgs = Batched::new(msgs, 10000);
-    let mut stdout = io::stdout();
-    let mut buf = BytesDeque::new();
-    'main: while let Some(m) = msgs.next().await {
-        match m {
-            BatchItem::InBatch((path, v)) => {
-                buf.push_back(path);
-                buf.push_back(fsep.clone());
-                buf.push_back(v);
-                buf.push_back(rsep.clone());
-            },
-            BatchItem::EndBatch => while buf.len() > 0 {
-                match stdout.write_buf(&mut buf).await {
-                    Ok(_) => (),
-                    Err(e) => {
-                        eprintln!("error writing to stdout: {}", e);
-                        break 'main
-                    }
-                }
-            }
-        }
-    }
-}
-
-enum Req {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum In {
     Add(Path),
     Drop(Path),
 }
 
-impl FromStr for Req {
+impl FromStr for In {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.starts_with("drop: ") && s.len() > 6 {
-            Ok(Req::Drop(Path::from(&s[6..])))
+        if let Ok(i) = serde_json::from_str(s) {
+            Ok(i)
+        } else if s.starts_with("DROP|") && s.len() > 5 {
+            Ok(In::Drop(Path::from(&s[5..])))
+        } else if s.starts_with("ADD|") && s.len() > 4 {
+            Ok(In::Add(Path::from(&s[4..])))
         } else {
             let p = Path::from(s);
             if !p.is_absolute() {
                 Err(format!("path is not absolute {}", p))
             } else {
-                Ok(Req::Add(p))
+                Ok(In::Add(p))
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Out {
+    path: Path,
+    value: Value,
+}
+
+impl Out {
+    fn write(&self, to_stdout: &mut BytesMut, to_stderr: &mut BytesMut) {
+        match serde_json::to_writer(&mut BytesWriter(&mut *to_stdout), self) {
+            Ok(()) => { to_stdout.extend_from_slice(b"\n") },
+            Err(e) => {
+                to_stderr.extend_from_slice(format!("{}|{}\n", self.path, e).as_ref());
             }
         }
     }
@@ -207,47 +124,82 @@ impl FromStr for Req {
 pub(crate) fn run(cfg: config::Resolver, paths: Vec<String>) {
     let mut rt = Runtime::new().expect("failed to init runtime");
     rt.block_on(async {
-        let mut subscriptions: HashMap::<Path, oneshot::Sender<()>> = HashMap::new();
+        let mut subscriptions: HashMap::<Path, DUVal> = HashMap::new();
         let subscriber = Subscriber::new(cfg).expect("create subscriber");
-        let (out_tx, out_rx) = mpsc::channel(100);
-        task::spawn(output_vals(out_rx));
-        let stdin = BufReader::new(io::stdin()).lines();
-        let mut requests = Batched::new(
-            stream::iter(paths).map(|p| Ok(p)).chain(stdin), 1000
-        );
+        let mut requests:
+        Box<dyn FusedStream<Item = BatchItem<Result<String, io::Error>>> + Unpin> = {
+            let stdin = BufReader::new(io::stdin()).lines();
+            Box::new(Batched::new(
+                stream::iter(paths).map(|p| Ok(p)).chain(stdin), 1000
+            ))
+        };
+        let mut updates:
+        Batched<SelectAll<Box<dyn Stream<Item = (Path, Bytes)> + Unpin>>> =
+            Batched::new(SelectAll::new(), 100_000);
+        let mut stdout = io::stdout();
+        let mut stderr = io::stderr();
+        let mut to_stdout = BytesMut::new();
+        let mut to_stderr = BytesMut::new();
         let mut add = HashSet::new();
         let mut drop = HashSet::new();
-        while let Some(l) = requests.next().await {
-            match l {
-                BatchItem::InBatch(l) => match try_brk!("reading", l).parse::<Req>() {
-                    Err(e) => eprintln!("{}", e),
-                    Ok(Req::Add(p)) => {
-                        if !drop.remove(&p) {
-                            add.insert(p);
+        // ensure that we never see None from updates.next()
+        updates.inner_mut().push(Box::new(stream::pending()));
+        loop {
+            select! {
+                u = updates.next() => match u {
+                    None => unreachable!(),
+                    Some(BatchItem::EndBatch) => {
+                        if to_stdout.len() > 0 {
+                            let to_write = to_stdout.split().freeze();
+                            try_brk!("write stdout", stdout.write_all(&*to_write).await);
+                        }
+                        if to_stderr.len() > 0 {
+                            let to_write = to_stderr.split().freeze();
+                            try_brk!("write stderr", stderr.write_all(&*to_write).await);
                         }
                     }
-                    Ok(Req::Drop(p)) => {
-                        if !add.remove(&p) {
-                            drop.insert(p);
+                    Some(BatchItem::InBatch((path, v))) => {
+                        let value = match rmpv::decode::value::read_value(&mut &*v) {
+                            Ok(v) => mpv_to_json(v),
+                            Err(_) => match from_utf8(&*v) {
+                                Ok(s) => Value::String(s.into()),
+                                Err(_) => Value::String(format!("{:?}", v)),
+                            }
+                        };
+                        Out {path, value}.write(&mut to_stdout, &mut to_stderr)
+                    }
+                },
+                r = requests.next() => match r {
+                    None | Some(BatchItem::InBatch(Err(_))) => {
+                        requests = Box::new(stream::pending());
+                    }
+                    Some(BatchItem::InBatch(Ok(l))) => match l.parse::<In>() {
+                        Err(e) => eprintln!("{}", e),
+                        Ok(In::Add(p)) => {
+                            if !drop.remove(&p) {
+                                add.insert(p);
+                            }
+                        }
+                        Ok(In::Drop(p)) => {
+                            if !add.remove(&p) {
+                                drop.insert(p);
+                            }
                         }
                     }
-                }
-                BatchItem::EndBatch => {
-                    for p in drop.drain() {
-                        subscriptions.remove(&p).into_iter().for_each(|s| {
-                            let _ = s.send(());
-                        })
-                    }
-                    for p in add.drain() {
-                        let (tx, rx) = oneshot::channel();
-                        subscriptions.insert(p.clone(), tx);
-                        let subscriber = subscriber.clone();
-                        task::spawn(run_subscription(p, subscriber, rx, out_tx.clone()));
+                    Some(BatchItem::EndBatch) => {
+                        for p in drop.drain() { subscriptions.remove(&p); }
+                        for p in add.drain() {
+                            subscriptions.entry(p.clone()).or_insert_with(|| {
+                                let s = subscriber.durable_subscribe_val_ut(p.clone());
+                                updates.inner_mut().push(Box::new(
+                                    s.updates(true).map(move |v| (p.clone(), v))
+                                ));
+                                s
+                            });
+                        }
                     }
                 }
             }
         }
-        // run until we are killed even if stdin closes
-        future::pending::<()>().await;
     });
 }
