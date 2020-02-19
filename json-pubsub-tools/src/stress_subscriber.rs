44498e96cd1f856565d7feb810792dc447639024
Eric Stokes <letaris@gmail.com>
2020-02-18 23:27:28 -0500

    add average rate to stress subscriber

--- json-pubsub-tools/src/stress_subscriber.rs
+++ json-pubsub-tools/src/stress_subscriber.rs
@@ -20,25 +20,30 @@ pub(crate) fn run(config: config::Resolver) {
             .collect::<Vec<DVal<Data>>>();
         let mut vals = stream::select_all(subs.iter().map(|s| s.updates(true)));
         let one_second = Duration::from_secs(1);
-        let mut last_stat = Instant::now();
+        let start = Instant::now();
+        let mut last_stat = start;
+        let mut total = 0;
         let mut n = 0;
         while let Some(_) = vals.next().await {
+            total += 1;
             n += 1;
             if n >= subs.len() {
                 let now = Instant::now();
                 let elapsed = now - last_stat;
-                let mut subscribed = 0;
-                let mut unsubscribed = 0;
-                for s in subs.iter() {
-                    match s.state() {
-                        DVState::Unsubscribed => { unsubscribed += 1; }
-                        DVState::Subscribed => { subscribed += 1; }
-                    }
-                }
+                let since_start = now - start;
                 if elapsed > one_second {
+                    let mut subscribed = 0;
+                    let mut unsubscribed = 0;
+                    for s in subs.iter() {
+                        match s.state() {
+                            DVState::Unsubscribed => { unsubscribed += 1; }
+                            DVState::Subscribed => { subscribed += 1; }
+                        }
+                    }
                     println!(
-                        "subscribed: {} unsubscribed: {} rx: {:.0}",
-                        subscribed, unsubscribed, n as f64 / elapsed.as_secs_f64()
+                        "subscribed: {} unsubscribed: {} rx: {:.0} rx(avg): {:.0}",
+                        subscribed, unsubscribed, n as f64 / elapsed.as_secs_f64(),
+                        total as f64 / since_start.as_secs_f64()
                     );
                     n = 0;
                     last_stat = now;
