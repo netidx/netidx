54dd19b585f62fc4176319e4a273237293168e67
Eric Stokes <letaris@gmail.com>
2020-05-11 00:44:47 -0400

    fix the tools for varints

--- json-pubsub-tools/src/publisher.rs
+++ json-pubsub-tools/src/publisher.rs
@@ -18,9 +18,13 @@ use tokio::{
 #[derive(Debug, Clone, Copy)]
 pub enum Typ {
     U32,
+    V32,
     I32,
+    Z32,
     U64,
+    V64,
     I64,
+    Z64,
     F32,
     F64,
     String,
@@ -33,9 +37,13 @@ impl FromStr for Typ {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "u32" => Ok(Typ::U32),
+            "v32" => Ok(Typ::V32),
             "i32" => Ok(Typ::I32),
+            "z32" => Ok(Typ::Z32),
             "u64" => Ok(Typ::U64),
+            "v64" => Ok(Typ::V64),
             "i64" => Ok(Typ::I64),
+            "z64" => Ok(Typ::Z64),
             "f32" => Ok(Typ::F32),
             "f64" => Ok(Typ::F64),
             "string" => Ok(Typ::String),
@@ -49,9 +57,13 @@ impl FromStr for Typ {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub(crate) enum SValue {
     U32(u32),
+    V32(u32),
     I32(i32),
+    Z32(i32),
     U64(u64),
+    V64(u64),
     I64(i64),
+    Z64(i64),
     F32(f32),
     F64(f64),
     String(String),
@@ -62,9 +74,13 @@ impl From<Value> for SValue {
     fn from(v: Value) -> Self {
         match v {
             Value::U32(n) => SValue::U32(n),
+            Value::V32(n) => SValue::V32(n),
             Value::I32(n) => SValue::I32(n),
+            Value::Z32(n) => SValue::Z32(n),
             Value::U64(n) => SValue::U64(n),
+            Value::V64(n) => SValue::V64(n),
             Value::I64(n) => SValue::I64(n),
+            Value::Z64(n) => SValue::Z64(n),
             Value::F32(n) => SValue::F32(n),
             Value::F64(n) => SValue::F64(n),
             Value::String(c) => SValue::String(String::from(c.as_ref())),
@@ -77,9 +93,13 @@ impl Into<Value> for SValue {
     fn into(self) -> Value {
         match self {
             SValue::U32(n) => Value::U32(n),
+            SValue::V32(n) => Value::V32(n),
             SValue::I32(n) => Value::I32(n),
+            SValue::Z32(n) => Value::Z32(n),
             SValue::U64(n) => Value::U64(n),
+            SValue::V64(n) => Value::V64(n),
             SValue::I64(n) => Value::I64(n),
+            SValue::Z64(n) => Value::Z64(n),
             SValue::F32(n) => Value::F32(n),
             SValue::F64(n) => Value::F64(n),
             SValue::String(s) => Value::String(Chars::from(s)),
@@ -91,9 +111,13 @@ impl Into<Value> for SValue {
 fn parse_val(typ: Typ, s: &str) -> Result<SValue> {
     Ok(match typ {
         Typ::U32 => SValue::U32(s.parse::<u32>()?),
+        Typ::V32 => SValue::V32(s.parse::<u32>()?),
         Typ::I32 => SValue::I32(s.parse::<i32>()?),
+        Typ::Z32 => SValue::Z32(s.parse::<i32>()?),
         Typ::U64 => SValue::U64(s.parse::<u64>()?),
+        Typ::V64 => SValue::V64(s.parse::<u64>()?),
         Typ::I64 => SValue::I64(s.parse::<i64>()?),
+        Typ::Z64 => SValue::Z64(s.parse::<i64>()?),
         Typ::F32 => SValue::F32(s.parse::<f32>()?),
         Typ::F64 => SValue::F64(s.parse::<f64>()?),
         Typ::String => SValue::String(String::from(s)),
