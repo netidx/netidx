affba7559d416e70a747da8da1b32f95486ded30
Eric Stokes <letaris@gmail.com>
2020-05-14 00:25:45 -0400

    tighten up checks in config

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,64 @@
+mod common_file {
+    use crate::{
+        chars::Chars, path::Path, protocol::resolver::v1::Referral as Pref, utils,
+    };
+    use anyhow::Result;
+    use std::{collections::HashMap, net::SocketAddr};
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub(super) struct Referral {
+        pub(super) path: String,
+        pub(super) ttl: u64,
+        pub(super) addrs: Vec<SocketAddr>,
+        pub(super) krb5_spns: HashMap<SocketAddr, String>,
+    }
+
+    impl Referral {
+        pub(super) fn check(self, us: Option<SocketAddr>) -> Result<Pref> {
+            let path = Path::from(self.path);
+            if !Path::is_absolute(&path) {
+                bail!("absolute referral path is required")
+            }
+            if self.addrs.is_empty() {
+                bail!("empty referral addrs")
+            }
+            for addr in &self.addrs {
+                utils::check_addr(addr.ip(), &[])?;
+                if addr.port() == 0 {
+                    bail!("non zero port required {:?}", addr);
+                }
+            }
+            if !self.krb5_spns.is_empty() {
+                for a in &self.addrs {
+                    if !self.krb5_spns.contains_key(a) {
+                        bail!("spn for server {:?} is required", a)
+                    }
+                }
+            }
+            if self.ttl == 0 {
+                bail!("ttl must be non zero");
+            }
+            if let Some(ref us) = us {
+                if self.addrs.contains(&us) {
+                    bail!("server may not be it's own parent");
+                }
+            }
+            Ok(Pref {
+                path,
+                ttl: self.ttl,
+                addrs: self.addrs,
+                krb5_spns: self
+                    .krb5_spns
+                    .into_iter()
+                    .map(|(a, s)| (a, Chars::from(s)))
+                    .collect(),
+            })
+        }
+    }
+}
+
 pub mod resolver_server {
-    use crate::{chars::Chars, path::Path, protocol::resolver::v1::Referral, utils};
+    use crate::{path::Path, protocol::resolver::v1::Referral, utils};
     use anyhow::Result;
     use serde_json::from_str;
     use std::{
@@ -16,51 +75,13 @@ pub mod resolver_server {
     };
 
     mod file {
-        use super::{Chars, Path, Referral as Pref};
-        use anyhow::Result;
-        use std::{collections::HashMap, net::SocketAddr};
+        use super::super::common_file::Referral;
+        use std::net::SocketAddr;
 
         #[derive(Debug, Clone, Serialize, Deserialize)]
         pub(super) enum Auth {
             Anonymous,
             Krb5 { spn: String, permissions: String },
-        }
-
-        #[derive(Debug, Clone, Serialize, Deserialize)]
-        pub(super) struct Referral {
-            pub(super) path: String,
-            pub(super) ttl: u64,
-            pub(super) addrs: Vec<SocketAddr>,
-            pub(super) krb5_spns: HashMap<SocketAddr, String>,
-        }
-
-        impl Referral {
-            pub(super) fn check(self) -> Result<Pref> {
-                let path = Path::from(self.path);
-                if !Path::is_absolute(&path) {
-                    bail!("absolute referral path is required")
-                }
-                if self.addrs.is_empty() {
-                    bail!("empty referral addrs")
-                }
-                if !self.krb5_spns.is_empty() {
-                    for a in &self.addrs {
-                        if !self.krb5_spns.contains_key(a) {
-                            bail!("spn for server {:?} is required", a)
-                        }
-                    }
-                }
-                Ok(Pref {
-                    path,
-                    ttl: self.ttl,
-                    addrs: self.addrs,
-                    krb5_spns: self
-                        .krb5_spns
-                        .into_iter()
-                        .map(|(a, s)| (a, Chars::from(s)))
-                        .collect(),
-                })
-            }
         }
 
         #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -105,6 +126,10 @@ pub mod resolver_server {
     impl Config {
         pub fn load<P: AsRef<FsPath>>(file: P) -> Result<Config> {
             let cfg: file::Config = from_str(&read_to_string(file)?)?;
+            utils::check_addr(cfg.addr.ip(), &[])?;
+            if cfg.addr.port() == 0 {
+                bail!("You must specify a non zero port {:?}", cfg.addr);
+            }
             let auth = match cfg.auth {
                 file::Auth::Anonymous => Auth::Anonymous,
                 file::Auth::Krb5 { spn, permissions } => {
@@ -112,14 +137,14 @@ pub mod resolver_server {
                     Auth::Krb5 { spn, permissions }
                 }
             };
-            let parent = cfg.parent.map(|r| r.check()).transpose()?;
+            let parent = cfg.parent.map(|r| r.check(Some(cfg.addr))).transpose()?;
             let children = {
                 let root = parent.as_ref().map(|r| r.path.as_ref()).unwrap_or("/");
                 let children = cfg
                     .children
                     .into_iter()
                     .map(|r| {
-                        let r = r.check()?;
+                        let r = r.check(Some(cfg.addr))?;
                         Ok((r.path.clone(), r))
                     })
                     .collect::<Result<BTreeMap<Path, Referral>>>()?;
@@ -128,7 +153,7 @@ pub mod resolver_server {
                         bail!("child paths much be under the root path {}", p)
                     }
                     if Path::levels(&*p) <= Path::levels(&*root) {
-                        bail!("child paths must be deeper than  the root {}", p);
+                        bail!("child paths must be deeper than the root {}", p);
                     }
                     let mut res = children.range::<str, (Bound<&str>, Bound<&str>)>((
                         Excluded(r.path.as_ref()),
@@ -145,10 +170,6 @@ pub mod resolver_server {
                 }
                 children
             };
-            utils::check_addr(cfg.addr.ip(), &[])?;
-            if cfg.addr.port() == 0 {
-                bail!("You must specify a non zero port {:?}", cfg.addr);
-            }
             Ok(Config {
                 parent,
                 children,
@@ -165,38 +186,47 @@ pub mod resolver_server {
 }
 
 pub mod resolver {
+    use crate::protocol::resolver::v1::Referral;
     use anyhow::Result;
     use serde_json::from_str;
-    use std::{
-        collections::{HashMap, HashSet},
-        convert::AsRef,
-        net::SocketAddr,
-        path::Path,
-    };
+    use std::{convert::AsRef, path::Path};
     use tokio::fs::read_to_string;
+
+    mod file {
+        use super::{super::common_file::Referral, Auth};
+
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        pub(super) struct Config {
+            pub(super) resolver: Referral,
+            pub(super) auth: Auth,
+        }
+    }
 
     #[derive(Debug, Clone, Serialize, Deserialize)]
     pub enum Auth {
         Anonymous,
-        Krb5 { target_spns: HashMap<SocketAddr, String> },
+        Krb5,
     }
 
-    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[derive(Debug, Clone)]
     pub struct Config {
-        pub servers: HashSet<SocketAddr>,
+        pub resolver: Referral,
         pub auth: Auth,
     }
 
     impl Config {
         pub async fn load<P: AsRef<Path>>(file: P) -> Result<Config> {
-            let cfg: Config = from_str(&read_to_string(file).await?)?;
+            let cfg: file::Config = from_str(&read_to_string(file).await?)?;
+            let cfg = Config { resolver: cfg.resolver.check()?, auth: cfg.auth };
             match cfg.auth {
-                Auth::Anonymous => (),
-                Auth::Krb5 { ref target_spns } => {
-                    for addr in cfg.servers.iter() {
-                        if !target_spns.contains_key(addr) {
-                            bail!("missing target spn for server {:?}", addr)
-                        }
+                Auth::Anonymous => {
+                    if !cfg.resolver.krb5_spns.is_empty() {
+                        bail!("auth is anonymous but spns are configured");
+                    }
+                }
+                Auth::Krb5 => {
+                    if cfg.resolver.krb5_spns.is_empty() {
+                        bail!("auth is krb5 but no spns configured");
                     }
                 }
             }
