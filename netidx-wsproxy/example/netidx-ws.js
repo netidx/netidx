bb9ceac01d436a1720a96e3dfda26be24b12f43e
Eric Stokes <eric@architect.gg>
2023-03-01 22:40:00 -0500

    progress

--- netidx-wsproxy/example/netidx-ws.js
+++ netidx-wsproxy/example/netidx-ws.js
@@ -1,28 +1,31 @@
 class Netidx {
     con = null;
     subs = new Map();
+    call_id = 0;
     pending_subs = [];
-    pending_calls = [];
-    pending_replies = [];
-
+    pending_replies = new Map();
+    waiting_calls = [];
+    waiting_subs = [];
+    
     constructor(url) {
         this.con = new WebSocket(url);
         this.con.addEventListener('open', (event) => {
-            while (this.pending_subs.length > 0) {
-                let sub = this.pending_subs.shift();
+            while (this.waiting_subs.length > 0) {
+                let sub = this.waiting_subs.shift();
+                this.pending_subs.push(sub);
                 this.con.send(JSON.stringify({'type': 'Subscribe', 'path': sub[0]}));
             }
-            while (this.pending_calls.len > 0) {
-                let call = this.pending_calls.shift();
+            while (this.waiting_calls.length > 0) {
+                let call = this.waiting_calls.shift();
                 this.con.send(JSON.stringify(call.args));
-                this.pending_replies.push(call.reply);
+                this.pending_replies.set(call.args.id, call.reply);
             }
         });
         this.con.addEventListener('message', (event) => {
             console.log("message from netidx", event.data);
             const ev = JSON.parse(event.data);
             if (ev.type == "Subscribed") {
-                let handler = this.pending_subs.pop()[1];
+                let handler = this.pending_subs.shift()[1];
                 let w = this.subs.get(ev.id);
                 if (w == undefined) {
                     this.subs.set(ev.id, [handler]);
@@ -44,39 +47,53 @@ class Netidx {
                         this.con.send(JSON.stringify({'type': 'Unsubscribe', 'id': ev.id }))
                     }
                 }
-            } else if (ev.type == "Called") {
-                if (this.pending_replies.length > 0) {
-                    let resolve = this.pending_replies.shift();
-                    resolve(ev.result);
+            } else if (ev.type == "CallSuccess") {
+                let reply = this.pending_replies.get(ev.id);
+                if(reply != undefined) {
+                    reply[0](ev.result);
+                    this.pending_replies.delete(ev.id);
+                }
+            } else if (ev.type = "CallFailed") {
+                let reply = this.pending_replies.get(ev.id);
+                if(reply != undefined) {
+                    reply[1](ev.error);
+                    this.pending_replies.delete(ev.id);
                 }
             }
         });
     }
 
     subscribe(path, handler) {
-        this.pending_subs.unshift([path, handler]);
         if(this.con.readyState == 1) {
+            this.pending_subs.push([path, handler]);
             this.con.send(JSON.stringify({ 'type': 'Subscribe', 'path': path }));
+        } else {
+            this.waiting_subs.push([path, handler]);
         }
     }
 
     call(path, args) {
         return new Promise((reply, reject) => {
-            let call = {'type': 'Call', 'path': path, 'args': args};
+            let id = this.call_id;
+            this.call_id += 1;
+            let call = {'type': 'Call', 'id': id, 'path': path, 'args': args };
             if (this.con.readyState == 1) {
-                this.pending_replies.push(reply);
+                this.pending_replies.set(id, [reply, reject]);
                 this.con.send(JSON.stringify(call));
             } else {
-                this.pending_calls.push({'args': call, 'reply': reply});
+                this.waiting_calls.push({
+                    'args': call,
+                    'reply': [reply, reject]
+                });
             }
         })
     }
 }
 
 nx = new Netidx("ws://127.0.0.1:4343/ws");
-nx.subscribe('/local/test', (v) => {
-     console.log(v);
-     return true
-});
+// nx.subscribe('/local/bench/0/0', (v) => {
+//      console.log(v);
+//      return true
+// });
 nx.call('/local/test-rpc', [['echo', {'type': 'U32', 'value': 42}]])
     .then((v) => console.log(v));
