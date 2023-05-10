8f5065ffba744c3c8f491a1994108b2f56d308a4
Michael Bacarella <m@bacarella.com>
2023-05-10 09:34:40 -0700

    Daemonize before starting async

--- netidx-tools/src/resolver_server.rs
+++ netidx-tools/src/resolver_server.rs
@@ -23,17 +23,22 @@ pub(crate) struct Params {
     id: usize,
 }
 
-pub(crate) async fn run(params: Params) -> Result<()> {
-    let config =
-        Config::load(params.config).context("failed to load resolver server config")?;
+#[tokio::main]
+pub(crate) async fn tokio_run(config: Config, params: Params) -> Result<()> {
+    let _server = Server::new(config, params.delay_reads, params.id)
+        .await
+        .context("starting server")?;
+    future::pending::<Result<()>>().await
+}
+
+pub(crate) fn run(params: Params) -> Result<()> {
+    let config = Config::load(params.config.clone())
+        .context("failed to load resolver server config")?;
     let member = &config.member_servers[params.id];
     if !params.foreground {
         let mut file = member.pid_file.clone();
         file.push_str(&format!("{}.pid", params.id));
         Daemonize::new().pid_file(file).start().context("failed to daemonize")?;
     }
-    let _server = Server::new(config, params.delay_reads, params.id)
-        .await
-        .context("starting server")?;
-    future::pending::<Result<()>>().await
+    tokio_run(config, params)
 }
