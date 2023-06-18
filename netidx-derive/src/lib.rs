f7fb28f85497f363493fb9395e6444f8ae6e1ca8
Eric Stokes <eric@architect.xyz>
2023-06-17 22:00:56 -0400

    support explicit tagging of enum cases

--- netidx-derive/src/lib.rs
+++ netidx-derive/src/lib.rs
@@ -1,40 +1,83 @@
-use proc_macro2::{TokenStream, TokenTree};
-use quote::{format_ident, quote};
+use proc_macro2::{token_stream, Delimiter, TokenStream, TokenTree};
+use quote::{format_ident, quote, ToTokens};
 use syn::{
     parse_macro_input, parse_quote, AttrStyle, Attribute, Data, DeriveInput, Field,
     Fields, GenericParam, Ident, Index,
 };
 
+fn parse_attr<R, F: FnMut(Ident, token_stream::IntoIter) -> R>(
+    att: &Attribute,
+    mut f: F,
+) -> Option<R> {
+    match att.style {
+        AttrStyle::Inner(_) => None,
+        AttrStyle::Outer => match att.path().segments.iter().next() {
+            None => None,
+            Some(seg) => match seg.ident.to_string().as_str() {
+                "pack" => {
+                    let tokens = att.meta.require_list().unwrap().tokens.clone();
+                    let mut iter = tokens.into_iter();
+                    match iter.next() {
+                        Some(TokenTree::Ident(i)) => Some(f(i, iter)),
+                        None | Some(_) => None,
+                    }
+                }
+                _ => None,
+            },
+        },
+    }
+}
+
 fn is_attr(att: &Attribute, allowed: &[&str], s: &str) -> bool {
     if !allowed.contains(&s) {
         panic!("BUG: attribute '{}' is not included in '{:?}'", s, allowed);
     }
-    match att.style {
-        AttrStyle::Inner(_) => false,
-        AttrStyle::Outer => {
-            if let Some(seg) = att.path().segments.iter().next() {
-                seg.ident.to_string() == "pack" && {
-                    let tokens = att.meta.require_list().unwrap().tokens.clone();
-                    match tokens.into_iter().next() {
-                        None => false,
-                        Some(TokenTree::Ident(i)) => {
-                            let attr = i.to_string();
-                            if !allowed.contains(&attr.as_str()) {
-                                panic!("invalid attribute '{}'", attr);
-                            }
-                            attr == s
-                        }
-                        Some(_) => false,
-                    }
-                }
-            } else {
-                false
-            }
+    parse_attr(att, |i, _| {
+        let i = i.to_string();
+        if !allowed.contains(&i.as_str()) {
+            panic!("invalid pack attribute '{}'", i)
         }
-    }
+        &i == s
+    })
+    .unwrap_or(false)
 }
 
-static FIELD_ATTRS: [&'static str; 3] = ["skip", "default", "other"];
+fn get_tag(att: &Attribute, tagged: &mut bool, allowed: &[&str]) -> Option<TokenStream> {
+    let res = parse_attr(att, |i, mut ts| {
+        let i = i.to_string();
+        if !allowed.contains(&i.as_str()) {
+            panic!("invalid attribute '{}'", i)
+        } else if i != "tag" {
+            return None;
+        }
+        match ts.next() {
+            None => panic!("tag expected an integer argument"),
+            Some(TokenTree::Group(g)) => {
+                if g.delimiter() != Delimiter::Parenthesis {
+                    panic!("syntax error, e.g. tag(42)")
+                }
+                match g.stream().into_iter().next() {
+                    None => panic!("tag expected an int literal argument"),
+                    Some(TokenTree::Literal(l)) => {
+                        Some(TokenStream::from(TokenTree::Literal(l)))
+                    }
+                    Some(_) => panic!("tag expected an int literal argument"),
+                }
+            }
+            Some(_) => panic!("tag expected an integer argument"),
+        }
+    })
+    .flatten();
+    if res.is_none() && *tagged {
+        panic!("all cases must be explicitly tagged or none must be")
+    } else {
+        *tagged |= res.is_some()
+    }
+    res
+}
+
+static ENUM_ATTRS: [&'static str; 2] = ["tag", "other"];
+static FIELD_ATTRS: [&'static str; 2] = ["skip", "default"];
 
 fn encoded_len(no_wrap: bool, input: &Data) -> TokenStream {
     match input {
@@ -223,76 +266,79 @@ fn encode(no_wrap: bool, input: &Data) -> TokenStream {
             Fields::Unit => panic!("unit structs are not supported by Pack"),
         },
         Data::Enum(en) => {
-            let cases = en.variants.iter().enumerate().map(|(i, v)| match &v.fields {
-                Fields::Named(f) => {
-                    let match_fields = f
-                        .named
-                        .iter()
-                        .filter(|f| {
-                            !f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
-                        })
-                        .map(|f| &f.ident);
-                    let pack_fields = f
-                        .named
-                        .iter()
-                        .filter(|f| {
-                            !f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
-                        })
-                        .map(|f| {
-                            let name = &f.ident;
-                            quote! {
-                                netidx_core::pack::Pack::encode(#name, buf)?
+            let mut tagged = false;
+            let cases = en.variants.iter().enumerate().map(|(i, v)| {
+		let tag = &v.ident;
+                let i = v
+                    .attrs
+                    .iter()
+                    .find_map(|a| get_tag(a, &mut tagged, &ENUM_ATTRS))
+                    .unwrap_or_else(|| Index::from(i).to_token_stream());
+		match &v.fields {
+                    Fields::Named(f) => {
+			let match_fields = f
+                            .named
+                            .iter()
+                            .filter(|f| {
+				!f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
+                            })
+                            .map(|f| &f.ident);
+			let pack_fields = f
+                            .named
+                            .iter()
+                            .filter(|f| {
+				!f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
+                            })
+                            .map(|f| {
+				let name = &f.ident;
+				quote! {
+                                    netidx_core::pack::Pack::encode(#name, buf)?
+				}
+                            });
+			quote! {
+                            Self::#tag { #(#match_fields),*, .. } => {
+				<u8 as netidx_core::pack::Pack>::encode(&#i, buf)?;
+				#(#pack_fields);*;
+				Ok(())
                             }
-                        });
-                    let tag = &v.ident;
-                    let i = Index::from(i);
-                    quote! {
-                        Self::#tag { #(#match_fields),*, .. } => {
-                            <u8 as netidx_core::pack::Pack>::encode(&#i, buf)?;
-                            #(#pack_fields);*;
-                            Ok(())
-                        }
+			}
                     }
-                }
-                Fields::Unnamed(f) => {
-                    let match_fields = f.unnamed.iter().enumerate().map(|(i, f)| {
-                        if f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip")) {
-                            format_ident!("_")
-                        } else {
-                            format_ident!("field{}", i)
-                        }
-                    });
-                    let pack_fields = f
-                        .unnamed
-                        .iter()
-                        .enumerate()
-                        .filter(|(_, f)| {
-                            !f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
-                        })
-                        .map(|(i, _)| {
-                            let name = format_ident!("field{}", i);
-                            quote! {
-                                netidx_core::pack::Pack::encode(#name, buf)?
+                    Fields::Unnamed(f) => {
+			let match_fields = f.unnamed.iter().enumerate().map(|(i, f)| {
+                            if f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip")) {
+				format_ident!("_")
+                            } else {
+				format_ident!("field{}", i)
                             }
-                        });
-                    let tag = &v.ident;
-                    let i = Index::from(i);
-                    quote! {
-                        Self::#tag(#(#match_fields),*) => {
-                            <u8 as netidx_core::pack::Pack>::encode(&#i, buf)?;
-                            #(#pack_fields);*;
-                            Ok(())
-                        }
+			});
+			let pack_fields = f
+                            .unnamed
+                            .iter()
+                            .enumerate()
+                            .filter(|(_, f)| {
+				!f.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "skip"))
+                            })
+                            .map(|(i, _)| {
+				let name = format_ident!("field{}", i);
+				quote! {
+                                    netidx_core::pack::Pack::encode(#name, buf)?
+				}
+                            });
+			quote! {
+                            Self::#tag(#(#match_fields),*) => {
+				<u8 as netidx_core::pack::Pack>::encode(&#i, buf)?;
+				#(#pack_fields);*;
+				Ok(())
+                            }
+			}
                     }
-                }
-                Fields::Unit => {
-                    let tag = &v.ident;
-                    let i = Index::from(i);
-                    quote! {
-                        Self::#tag => <u8 as netidx_core::pack::Pack>::encode(&#i, buf),
+                    Fields::Unit => {
+			quote! {
+                            Self::#tag => <u8 as netidx_core::pack::Pack>::encode(&#i, buf),
+			}
                     }
-                }
-            });
+		}
+	    });
             if no_wrap {
                 quote! {
                     match self {
@@ -415,57 +461,60 @@ fn decode(no_wrap: bool, input: &Data) -> TokenStream {
                 .variants
                 .iter()
                 .enumerate()
-                .map(|(i, v)| match &v.fields {
-                    Fields::Named(f) => {
-                        if v.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "other")) {
-                            panic!("other attribute must be applied to a unit variant")
-                        }
-                        let name_fields = f.named.iter().map(|f| &f.ident);
-                        let decode_fields = f.named.iter().map(decode_named_field);
-                        let tag = &v.ident;
-                        let i = Index::from(i);
-                        quote! {
-                            #i => {
-                                #(#decode_fields);*;
-                                Ok(Self::#tag { #(#name_fields),* })
+                .map(|(i, v)| {
+		    let mut tagged = false;
+                    let tag = &v.ident;
+                    let i = v
+			.attrs
+			.iter()
+			.find_map(|a| get_tag(a, &mut tagged, &ENUM_ATTRS))
+			.unwrap_or_else(|| Index::from(i).to_token_stream());
+		    match &v.fields {
+			Fields::Named(f) => {
+                            if v.attrs.iter().any(|a| is_attr(a, &ENUM_ATTRS, "other")) {
+				panic!("other attribute must be applied to a unit variant")
                             }
-                        }
-                    }
-                    Fields::Unnamed(f) => {
-                        if v.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "other")) {
-                            panic!("other attribute must be applied to a unit variant")
-                        }
-                        let name_fields = f
-                            .unnamed
-                            .iter()
-                            .enumerate()
-                            .map(|(i, _)| format_ident!("field{}", i));
-                        let decode_fields = f
-                            .unnamed
-                            .iter()
-                            .enumerate()
-                            .map(|(i, f)| decode_unnamed_field(f, i));
-                        let tag = &v.ident;
-                        let i = Index::from(i);
-                        quote! {
-                            #i => {
-                                #(#decode_fields);*;
-                                Ok(Self::#tag(#(#name_fields),*))
+                            let name_fields = f.named.iter().map(|f| &f.ident);
+                            let decode_fields = f.named.iter().map(decode_named_field);
+                            quote! {
+				#i => {
+                                    #(#decode_fields);*;
+                                    Ok(Self::#tag { #(#name_fields),* })
+				}
                             }
-                        }
-                    }
-                    Fields::Unit => {
-                        let tag = &v.ident;
-                        let i = Index::from(i);
-                        if v.attrs.iter().any(|a| is_attr(a, &FIELD_ATTRS, "other")) {
-                            if other.is_some() {
-                                panic!("other attribute may be specified at most once")
+			}
+			Fields::Unnamed(f) => {
+                            if v.attrs.iter().any(|a| is_attr(a, &ENUM_ATTRS, "other")) {
+				panic!("other attribute must be applied to a unit variant")
                             }
-                            other = Some(quote! { _ => Ok(Self::#tag) })
-                        }
-                        quote! { #i => Ok(Self::#tag), }
+                            let name_fields = f
+				.unnamed
+				.iter()
+				.enumerate()
+				.map(|(i, _)| format_ident!("field{}", i));
+                            let decode_fields = f
+				.unnamed
+				.iter()
+				.enumerate()
+				.map(|(i, f)| decode_unnamed_field(f, i));
+                            quote! {
+				#i => {
+                                    #(#decode_fields);*;
+                                    Ok(Self::#tag(#(#name_fields),*))
+				}
+                            }
+			}
+			Fields::Unit => {
+                            if v.attrs.iter().any(|a| is_attr(a, &ENUM_ATTRS, "other")) {
+				if other.is_some() {
+                                    panic!("other attribute may be specified at most once")
+				}
+				other = Some(quote! { _ => Ok(Self::#tag) })
+                            }
+                            quote! { #i => Ok(Self::#tag), }
+			}
                     }
-                })
+		})
                 .collect::<Vec<_>>();
             if no_wrap {
                 match other {
