826ef23b5146c35c92bce0de8f970768a0261caf
MegaPikachu <1025151692@qq.com>
2024-01-16 18:10:49 +0800

    add timeout setting for wsproxy

--- netidx-wsproxy/src/config.rs
+++ netidx-wsproxy/src/config.rs
@@ -7,6 +7,9 @@ pub struct Config {
     #[structopt(long = "listen", help = "the websocket address/port to listen on")]
     pub listen: String,
     #[serde(default)]
+    #[structopt(long = "wstimeout", help = "the websocket timeout, unit: sec. example: 3.0")]
+    pub wstimeout: Option<f64>,
+    #[serde(default)]
     #[structopt(long = "cert", help = "path to the tls certificate")]
     pub cert: Option<String>,
     #[serde(default)]
