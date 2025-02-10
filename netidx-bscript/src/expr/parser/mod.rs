f7abe2b4ed888dd4d95f9125b0287382a664e75e
Eric Stokes <eestokes@pm.me>
2025-02-10 10:32:18 -0500

    add _ to the list of reserved type names

--- netidx-bscript/src/expr/parser/mod.rs
+++ netidx-bscript/src/expr/parser/mod.rs
@@ -40,6 +40,7 @@ pub const TYPE_RESERVED: LazyLock<FxHashSet<&str>> = LazyLock::new(|| {
     FxHashSet::from_iter([
         "u32", "v32", "i32", "z32", "u64", "v64", "i64", "z64", "f32", "f64", "decimal",
         "datetime", "duration", "bool", "string", "bytes", "result", "array", "null",
+        "_",
     ])
 });
 
