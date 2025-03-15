4009f91e6d43bc7361feca44c69f27a7ca2b0850
Eric Stokes <eestokes@pm.me>
2025-03-15 17:13:57 -0400

    improve implementation of map, and implement fold

--- netidx-bscript/src/stdfn/core.rs
+++ netidx-bscript/src/stdfn/core.rs
@@ -13,10 +13,11 @@ use anyhow::bail;
 use anyhow::Result;
 use arcstr::{literal, ArcStr};
 use compact_str::format_compact;
+use fxhash::FxHashMap;
 use netidx::{publisher::Typ, subscriber::Value};
 use netidx_netproto::valarray::ValArray;
 use smallvec::{smallvec, SmallVec};
-use std::sync::Arc;
+use std::{mem, sync::Arc};
 
 struct IsErr;
 
@@ -680,14 +681,13 @@ run!(filter, FILTER, |v: Result<&Value>| {
 });
 
 macro_rules! mapfn {
-    ($name:ident, $bname:literal, $typ:literal) => {
+    ($name:ident, $bname:literal, $typ:literal, $buf:literal) => {
         struct $name<C: Ctx, E: UserEvent> {
             pred: Box<dyn Apply<C, E> + Send + Sync>,
             x: BindId,
             typ: FnType<NoRefs>,
             from: [Node<C, E>; 1],
-            queue: SmallVec<[usize; 4]>,
-            buf: SmallVec<[Value; 32]>,
+            buf: SmallVec<[Value; $buf]>,
         }
 
         impl<C: Ctx, E: UserEvent> BuiltIn<C, E> for $name<C, E> {
@@ -712,7 +712,6 @@ macro_rules! mapfn {
                             x,
                             from,
                             typ: typ.clone(),
-                            queue: smallvec![],
                             buf: smallvec![],
                         }))
                     }
@@ -728,22 +727,28 @@ macro_rules! mapfn {
                 from: &mut [Node<C, E>],
                 event: &mut Event<E>,
             ) -> Option<Value> {
-                if let Some(Value::Array(a)) = from[0].update(ctx, event) {
-                    self.queue.push(a.len());
-                    for v in a.iter() {
-                        ctx.user.set_var(self.x, v.clone());
+                match from[0].update(ctx, event) {
+                    Some(Value::Array(a)) if a.len() == 0 => {
+                        self.predicate(ctx, false, event);
+                        self.finish()
+                    }
+                    Some(Value::Array(a)) => {
+                        let mut variables = FxHashMap::default();
+                        for (i, v) in a.iter().enumerate() {
+                            event.variables.insert(self.x, v.clone());
+                            self.predicate(ctx, true, event);
+                            if i == 0 {
+                                variables = mem::take(&mut event.variables);
+                            }
+                        }
+                        event.variables = variables;
+                        self.finish()
+                    }
+                    Some(_) | None => {
+                        self.predicate(ctx, false, event);
+                        None
                     }
                 }
-                let set = event.variables.contains_key(&self.x);
-                self.predicate(ctx, set, event);
-                if set && !self.queue.is_empty() {
-                    self.queue[0] -= 1;
-                    if self.queue[0] == 0 {
-                        self.queue.remove(0);
-                        return self.finish();
-                    }
-                }
-                None
             }
 
             fn typecheck(
@@ -772,7 +777,7 @@ macro_rules! mapfn {
     };
 }
 
-mapfn!(ArrayFilter, "array_filter", "fn(Array<'a>, fn('a) -> bool) -> Array<'a>");
+mapfn!(ArrayFilter, "array_filter", "fn(Array<'a>, fn('a) -> bool) -> Array<'a>", 32);
 
 impl<C: Ctx, E: UserEvent> ArrayFilter<C, E> {
     fn predicate(&mut self, ctx: &mut ExecCtx<C, E>, set: bool, event: &mut Event<E>) {
@@ -811,7 +816,7 @@ run!(array_filter, ARRAY_FILTER, |v: Result<&Value>| {
     }
 });
 
-mapfn!(ArrayMap, "array_map", "fn(Array<'a>, fn('a) -> 'b) -> Array<'b>");
+mapfn!(ArrayMap, "array_map", "fn(Array<'a>, fn('a) -> 'b) -> Array<'b>", 32);
 
 impl<C: Ctx, E: UserEvent> ArrayMap<C, E> {
     fn predicate(&mut self, ctx: &mut ExecCtx<C, E>, set: bool, event: &mut Event<E>) {
@@ -851,7 +856,8 @@ run!(array_map, ARRAY_MAP, |v: Result<&Value>| {
 mapfn!(
     ArrayFilterMap,
     "array_filter_map",
-    "fn(Array<'a>, fn('a) -> ['b, null]) -> Array<'b>"
+    "fn(Array<'a>, fn('a) -> ['b, null]) -> Array<'b>",
+    32
 );
 
 impl<C: Ctx, E: UserEvent> ArrayFilterMap<C, E> {
@@ -891,17 +897,15 @@ run!(array_filter_map, ARRAY_FILTER_MAP, |v: Result<&Value>| {
     }
 });
 
-mapfn!(ArrayFind, "array_find", "fn(Array<'a>, fn('a) -> bool) -> ['a, null]");
+mapfn!(ArrayFind, "array_find", "fn(Array<'a>, fn('a) -> bool) -> ['a, null]", 1);
 
 impl<C: Ctx, E: UserEvent> ArrayFind<C, E> {
     fn predicate(&mut self, ctx: &mut ExecCtx<C, E>, set: bool, event: &mut Event<E>) {
-        if self.buf.is_empty() {
-            match self.pred.update(ctx, &mut self.from, event) {
-                Some(Value::Bool(true)) if set => {
-                    self.buf.push(event.variables[&self.x].clone())
-                }
-                _ => (),
+        match self.pred.update(ctx, &mut self.from, event) {
+            Some(Value::Bool(true)) if set && self.buf.is_empty() => {
+                self.buf.push(event.variables[&self.x].clone())
             }
+            _ => (),
         }
     }
 
@@ -933,17 +937,16 @@ run!(array_find, ARRAY_FIND, |v: Result<&Value>| {
 mapfn!(
     ArrayFindMap,
     "array_find_map",
-    "fn(Array<'a>, fn('a) -> ['b, null]) -> ['b, null]"
+    "fn(Array<'a>, fn('a) -> ['b, null]) -> ['b, null]",
+    1
 );
 
 impl<C: Ctx, E: UserEvent> ArrayFindMap<C, E> {
     fn predicate(&mut self, ctx: &mut ExecCtx<C, E>, set: bool, event: &mut Event<E>) {
-        if self.buf.is_empty() {
-            match self.pred.update(ctx, &mut self.from, event) {
-                Some(Value::Null) => (),
-                Some(v) if set => self.buf.push(v),
-                _ => (),
-            }
+        match self.pred.update(ctx, &mut self.from, event) {
+            Some(Value::Null) => (),
+            Some(v) if set && self.buf.is_empty() => self.buf.push(v),
+            _ => (),
         }
     }
 
@@ -968,6 +971,137 @@ const ARRAY_FIND_MAP: &str = r#"
 run!(array_find_map, ARRAY_FIND_MAP, |v: Result<&Value>| {
     match v {
         Ok(Value::I64(2)) => true,
+        _ => false,
+    }
+});
+
+struct ArrayFold<C: Ctx, E: UserEvent> {
+    pred: Box<dyn Apply<C, E> + Send + Sync>,
+    x: BindId,
+    y: BindId,
+    typ: FnType<NoRefs>,
+    from: [Node<C, E>; 2],
+    cached: Option<ValArray>,
+    init: Option<Value>,
+}
+
+impl<C: Ctx, E: UserEvent> BuiltIn<C, E> for ArrayFold<C, E> {
+    const NAME: &str = "array_fold";
+    deftype!("fn(Array<'a>, 'b, fn('b, 'a) -> 'b) -> 'b");
+
+    fn init(_: &mut ExecCtx<C, E>) -> BuiltInInitFn<C, E> {
+        Arc::new(|ctx, typ, scope, from, top_id| match from {
+            [_, _, Node { spec: _, typ: _, kind: NodeKind::Lambda(lb) }] => {
+                let x = ctx.env.bind_variable(scope, "x", from[0].typ.clone()).id;
+                let y = ctx.env.bind_variable(scope, "y", from[1].typ.clone()).id;
+                ctx.user.ref_var(x, top_id);
+                ctx.user.ref_var(y, top_id);
+                let mut from = [
+                    Node {
+                        spec: Box::new(ExprKind::Ref { name: ["x"].into() }.to_expr()),
+                        typ: Type::empty_tvar(),
+                        kind: NodeKind::Ref(x),
+                    },
+                    Node {
+                        spec: Box::new(ExprKind::Ref { name: ["y"].into() }.to_expr()),
+                        typ: Type::empty_tvar(),
+                        kind: NodeKind::Ref(y),
+                    },
+                ];
+                let pred = (lb.init)(ctx, &mut from, top_id)?;
+                Ok(Box::new(Self {
+                    pred,
+                    x,
+                    y,
+                    from,
+                    typ: typ.clone(),
+                    cached: None,
+                    init: None,
+                }))
+            }
+            _ => bail!("expected a function"),
+        })
+    }
+}
+
+impl<C: Ctx, E: UserEvent> Apply<C, E> for ArrayFold<C, E> {
+    fn update(
+        &mut self,
+        ctx: &mut ExecCtx<C, E>,
+        from: &mut [Node<C, E>],
+        event: &mut Event<E>,
+    ) -> Option<Value> {
+        if let Some(Value::Array(a)) = from[0].update(ctx, event) {
+            self.cached = Some(a);
+        }
+        if let Some(init) = from[1].update(ctx, event) {
+            self.init = Some(init)
+        }
+        match (&self.cached, &self.init) {
+            (Some(a), Some(init)) if a.len() == 0 => {
+                let _ = self.pred.update(ctx, &mut self.from, event);
+                Some(init.clone())
+            }
+            (Some(a), Some(init)) => {
+                let mut acc = init.clone();
+                let mut variables = FxHashMap::default();
+                for (i, v) in a.iter().enumerate() {
+                    event.variables.insert(self.x, acc.clone());
+                    event.variables.insert(self.y, v.clone());
+                    if let Some(v) = self.pred.update(ctx, &mut self.from, event) {
+                        acc = v;
+                    }
+                    if i == 0 {
+                        variables = mem::take(&mut event.variables);
+                    }
+                }
+                event.variables = variables;
+                Some(acc)
+            }
+            (None, _) | (_, None) => {
+                let _ = self.pred.update(ctx, &mut self.from, event);
+                None
+            }
+        }
+    }
+
+    fn typecheck(
+        &mut self,
+        ctx: &mut ExecCtx<C, E>,
+        from: &mut [Node<C, E>],
+    ) -> anyhow::Result<()> {
+        for n in from.iter_mut() {
+            n.typecheck(ctx)?;
+        }
+        let et = self.from[1].typ.clone();
+        Type::Array(triomphe::Arc::new(et)).check_contains(&from[0].typ)?;
+        self.from[0].typ.check_contains(&from[1].typ)?;
+        for n in self.from.iter_mut() {
+            n.typecheck(ctx)?;
+        }
+        self.pred.typecheck(ctx, &mut self.from[..])?;
+        match self.typ.args.get(2) {
+            Some(FnArgType { label: _, typ: Type::Fn(ft) }) => {
+                ft.rtype.check_contains(self.pred.rtype())?
+            }
+            _ => bail!("expected function as 2nd arg"),
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+const ARRAY_FOLD: &str = r#"
+{
+  let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  array::fold(a, 0, |acc, x| x + acc)
+}
+"#;
+
+#[cfg(test)]
+run!(array_fold, ARRAY_FOLD, |v: Result<&Value>| {
+    match v {
+        Ok(Value::I64(55)) => true,
         _ => false,
     }
 });
@@ -1416,6 +1550,7 @@ pub fn register<C: Ctx, E: UserEvent>(ctx: &mut ExecCtx<C, E>) -> Expr {
     ctx.register_builtin::<ArrayFind<C, E>>().unwrap();
     ctx.register_builtin::<ArrayFindMap<C, E>>().unwrap();
     ctx.register_builtin::<ArrayMap<C, E>>().unwrap();
+    ctx.register_builtin::<ArrayFold<C, E>>().unwrap();
     ctx.register_builtin::<ArrayFilterMap<C, E>>().unwrap();
     ctx.register_builtin::<FilterErr>().unwrap();
     ctx.register_builtin::<Group<C, E>>().unwrap();
